1ac886bb7d435da8556b60cc2c36e6defda4dcf7
jumpei00 <bs.albireo@gmail.com>
2020-07-11 20:41:24 +0900

    master

---
+++ app/models/database/base.py
@@ -0,0 +1,34 @@
+from contextlib import contextmanager
+import logging
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
+
+import settings
+
+
+logger = logging.getLogger(__name__)
+Base = declarative_base()
+engine = create_engine(f'sqlite:///{settings.db_name}')
+Session = scoped_session(sessionmaker(bind=engine))
+
+
+@contextmanager
+def session_scope():
+    session = Session()
+    session.expire_on_commit = False
+    try:
+        yield session
+        session.commit()
+    except Exception as e:
+        logger.error(f'action=session_scope error={e}')
+        session.rollback()
+        raise
+    finally:
+        session.expire_on_commit = True
+
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
